bc3383d2183f4df4d759c6b0dcd682835b28bdb9
wyenelle <favourkcb@gmail.com>
2023-03-15 23:29:44 +0100

    designing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,55 @@
 <script setup>
-import {RouterLink} from 'vue-router'
+import { RouterLink } from 'vue-router'
+import Img from '../assets/calc.png'
 
 </script>
 
 <template>
-    <main class="wrapper  h-screen centered-col main-bg gap-3">
-        <div class="w-full md:w-11/12 py-5 md:py-4 flex justify-between items-center px-2 md:px-0 ">
-            <h1 class="text-2xl font-bold"> Ayomipo</h1>
+    <main class="wrapper  h-auto  main-bg  md:px-5 md:py-3">
+        <div class="w-full  text-white py-6 md:py-4 flex justify-end  items-center px-5  ">
             <RouterLink to="/counter" class="text-2xl font-bold"> Counter</RouterLink>
         </div>
 
-        <div class="w-full md:w-11/12 md:h-5/6  h-full ">
-            
-            <div class=" grid grid-cols-2 w-full h-full ">
+        <div class="w-full mt-10  h-full ">
+
+            <div class=" grid grid-cols-2 text-white w-full h-full ">
                 <div class="col-span-2 md:col-span-1 ">
-                    <div class="w-full h-full centered-col gap-5 md:items-start">
-                        <h1 class="w-9/12 text-4xl font-bold text-center md:text-start ">thinking of using a proven <span class="text-white">counter </span>  ? i've got you covered. </h1>
-    
-                        <p class="w-10/12 text-xl text-center md:text-start">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nulla, veritatis adipisci mollitia rem tenetur!
+                    <div class="w-full h-full centered-col gap-9 pl-6">
+                        <h1 class="w-10/12  text-4xl font-bold text-start ">thinking of using a good and proven <span
+                                class="text-orange-500">counter </span> ? i've got you covered. </h1>
+
+                        <p class="w-10/12 text-xl ">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nulla, veritatis adipisci mollitia
+                            rem tenetur!
                         </p>
-    
-                        <ul class="mt-5 ml-2  bg-white/20 pl-5 italic  md:px-5  shadow-lg py-3 px-3">
-                           <li class="text-gray-600 text-xl">Add and Subtract functionality</li> 
-                           <li class="text-gray-600 text-xl">Use of Composables</li> 
-                           <li class="text-gray-600 text-xl">Set counter</li> 
-                           <li class="text-gray-600 text-xl">Reset</li> 
-                           <li class="text-gray-600 text-xl">Vuex</li> 
+
+                        <ul class="md:mt-5  mt-3   italic    shadow-lg  ">
+                            <li class="- text-xl">Add and Subtract functionality</li>
+                            <li class="- text-xl">Use of Composables</li>
+                            <li class="- text-xl">Set counter</li>
+                            <li class="- text-xl">Reset</li>
+                            <li class="- text-xl">Vuex</li>
                         </ul>
-    
-                        <RouterLink to="/counter">
+
+                        <RouterLink to="/counter" class="hidden md:block">
                             <button class="btn"> Explore</button>
                         </RouterLink>
                     </div>
                 </div>
-                <div class="col-span-1  hidden front md:block ">
-                    
+                <div class="col-span-2 md:col-span-1    ">
+                    <img :src="Img" alt="photo" class="" />
                 </div>
-           </div>
+            </div>
+
+        </div>
+        <div class="mt-10"></div>
+        <RouterLink to="/counter" class="md:hidden pl-6 mt-10 ">
+            <button class="btn"> Explore</button>
+        </RouterLink>
+        <div class="h-40 ">
+
         </div>
     </main>
 </template>
 
-<style scoped>
-.main-bg{
-    background-image:linear-gradient(to bottom right, #eebd89,#d13abd) ;
-}
-.front{
-    background: url('../assets/front.png'); 
-    background-size: cover;
-    background-position: center;
-}
-
-</style>
+<style scoped></style>
--- src/views/counter.vue
+++ src/views/counter.vue
@@ -1,37 +1,42 @@
 <script setup>
 import useCounter from '../composable/useCount.js'
-import {useStore} from 'vuex'
-import {computed} from 'vue'
-import {useState} from 'vuex-composition-helpers/dist'
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+import { useState } from 'vuex-composition-helpers/dist'
 
 
 const store = useStore()
-const {count} = useState(['count'])
+const { count } = useState(['count'])
 
 
 let num = ''
-const {increment,decrement,reset,setValue} = useCounter(num,store)
+const { increment, decrement, reset, setValue } = useCounter(num, store)
 </script>
 
 <template>
-    <div class="counter">
-        <h1>Counter Page</h1> 
-        <div class="input">
-            <input type="text" class="" placeholder="set value" v-model="num" />
-            <button @click="setValue(num)">Add</button>
-        </div>
-        
-        <div class="number">
-            <h1> {{ count }}</h1>
-        </div>
-        
-        <div>
-            <div class="functions">
+    <div class="h-screen border-4 border-red-500">
+        <h1 class="text-white w-6/12 mx-auto mt-8 text-center text-3xl">Counter Page</h1>
+
+        <div class="flex justify-center items-center flex-col mt-10 gap-5 h-5/6 border-4 border-red-500 text-white">
+            <div class="h-40 border-4 border-orange-600 rounded-md centered mb-3 ">
+                <h1 class="text-6xl font-extrabold px-2 text-white"> {{ count }}</h1>
+            </div>
+
+            <div class="input">
+                <input type="text" class="" placeholder="set value" v-model="num" />
+                <button @click="setValue(num)">Add</button>
+            </div>
+
+
+
+            <div>
+                <div class="functions">
                     <button @click="decrement" class="-">Decrement</button>
                     <button @click="reset" class="reset">Reset</button>
                     <button @click="increment" class="-">Increment</button>
+                </div>
             </div>
-        </div> 
+        </div>
 
         <div class="home">
             <router-link to='/' class="home-text">
@@ -41,103 +46,4 @@ const {increment,decrement,reset,setValue} = useCounter(num,store)
     </div>
 </template>
 
-<style scoped>
-
-    .counter{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin:auto;
-        padding:2rem;
-        background-color: black;
-        color: white;
-    }
-    .input input{
-        height: 30px;        
-        border-radius: 5px;
-        border: 1px solid gray;
-        margin-right: 2px;
-    }
-    .input{
-        margin-top: 3rem;
-    }
-    .input button{
-        height: 30px;
-        width: 70px;
-        text-align: center;
-        background-color: green;
-        color:white;
-        font-weight: bolder;
-        font-size: 18px;
-        font-family:  'Alegreya', serif;
-        padding: 5px;
-        border: 1px solid gray ;
-    }
-    .number{
-        height: 100px;
-        width: 100px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        color:white;
-        margin: 2rem 0;
-        font-weight: 500;
-        font-size: 28px;
-    }
-.functions{
-display: flex;
-justify-content: space-between;
-width:90vw;
-align-items: center;
-gap: 1rem;
-margin-top: 1.3rem;
-}
-.functions button{
-    padding: 10px;
-    border-radius: 7px;
-    background-color: green;
-    color:white;
-    font-weight: 700;
-    font-size: 20px;
-    border: 1px solid green;
-    outline: none;
-    font-family: 'Alegreya', serif;
-}
-.functions .reset{
-    background-color: red;
-}
-.home{
-    border: 2px solid white;
-    color: white;
-    width: 50vw;
-    height: 3rem;
-    padding: 5px;
-    text-align: center;
-    border-radius: 7px;
-    margin-top: 4rem;
-}
-.home .home-text{
-    color:white;
-    font-size: 22px;
-    font-weight: 700;
-    text-decoration: none;
-    font-family: 'Alegreya', serif;
-
-}
-@media (min-width: 768px){
-    .counter{
-        width: 70vw;
-        height: 90vh;
-        margin-top: 2em;
-    }
-    .functions{
-        width: 30vw;
-    }
-    .home{
-        width: 25vw;
-    }
-}
-</style>
+<style scoped></style>
